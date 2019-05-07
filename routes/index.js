d5be500d00a00c988b933c76d10fb0a6846f4d7e
lionel <lioneltolosaarmas@gmail.com>
2019-05-07 12:48:32 +0200

    clean up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var Kitchen = require('../models/kitchen');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/kitchen', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+router.get('/kitchen', (req, res, next) => {
+
+    Kitchen.find({})
+        .exec(
+            (err, recetas) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error cargando recetas',
+                        errors: err
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    recetas: recetas
+                });
+            });
 });
 
 module.exports = router;
