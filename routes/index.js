2afafa218221e29b7d62701207c6d6e945b23988
lionel <lioneltolosaarmas@gmail.com>
2019-05-07 13:07:58 +0200

    Clean

--- routes/index.js
+++ routes/index.js
@@ -4,65 +4,8 @@ var app = express();
 var Kitchen = require('../models/kitchen');
 
 /* GET home page. */
-// app.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-
-app.get('/', (req, res, next) => {
-
-    Kitchen.find({})
-        .exec(
-            (err, recetas) => {
-                if (err) {
-                    return res.status(500).json({
-                        ok: false,
-                        mensaje: 'Error cargando recetas',
-                        errors: err
-                    });
-                }
-
-                res.status(200).json({
-                    ok: true,
-                    recetas: recetas
-                });
-            });
-});
-
-app.post('/', (req, res) => {
-
-    var body = req.body;
-
-    var kitchen = new Kitchen({
-        name: body.name,
-        image: body.image,
-        assessment: body.assessment,
-        information: body.information,
-        difficulty: body.difficulty,
-        time: body.time,
-        ingredients: body.ingredients,
-        steps: body.steps,
-        tips: body.tips,
-    });
-
-    kitchen.save((err, kitchenGuardado) => {
-
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'Error al crear usuario',
-                errors: err
-            });
-        }
-
-        res.status(201).json({
-            ok: true,
-            kitchen: kitchenGuardado
-        });
-
-
-    });
-
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = app;
